9802875c84ed41d3c32b43a6fbcae4d3c69e3146
Patrick Lang <patrick.lang@hotmail.com>
2018-06-02 19:54:54 -0700

    Adding URL redirection controller

---
+++ shorturl/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace shorturl.Controllers
+{
+    [Route("/")]
+    public class DefaultController : Controller
+    {
+        private Dictionary<string, string> testValues;
+
+        public DefaultController()
+        {
+            testValues = new Dictionary<string, string>();
+            testValues.Add("bs", "http://beersmith.com/");
+            testValues.Add("bx", "http://beerxml.com/");
+        }
+    }
+}
