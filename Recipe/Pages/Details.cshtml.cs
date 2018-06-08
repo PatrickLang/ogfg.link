215d2dea51dedc10d5da7e417a9b918b7f0f277e
Patrick Lang <patrick.lang@hotmail.com>
2018-06-07 23:28:08 -0700

    Adding CRUD for recipe links

---
+++ Recipe/Pages/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ogfg.recipeapp.Data;
+using ogfg.recipeapp.Models;
+
+namespace ogfg.recipeapp.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ogfg.recipeapp.Data.RecipeContext _context;
+
+        public DetailsModel(ogfg.recipeapp.Data.RecipeContext context)
+        {
+            _context = context;
+        }
+
+        public Recipe Recipe { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Recipe = await _context.Recipes.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (Recipe == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
--- Recipe/Pages/Index.cshtml.cs
+++ Recipe/Pages/Index.cshtml.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ogfg.recipeapp.Data;
+using ogfg.recipeapp.Models;
 
 namespace ogfg.recipeapp.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ogfg.recipeapp.Data.RecipeContext _context;
 
+        public IndexModel(ogfg.recipeapp.Data.RecipeContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Recipe> Recipe { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Recipe = await _context.Recipes.ToListAsync();
         }
     }
 }
