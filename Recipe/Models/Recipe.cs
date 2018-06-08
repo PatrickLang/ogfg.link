fccffaa47e3ec7ec38621ec8fbec6a330cbf70f2
Patrick Lang <patrick.lang@hotmail.com>
2018-06-07 23:10:48 -0700

    Fixing primary key

--- Recipe/Models/Recipe.cs
+++ Recipe/Models/Recipe.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace ogfg.recipeapp.Models
 {
     public class Recipe
     {
+        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public Guid Id { get; set; }
         public string Name { get; set; }
