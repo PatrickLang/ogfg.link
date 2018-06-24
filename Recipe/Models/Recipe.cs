d254438b1b89f56a43fd582a1af0fb07af3b27d0
Patrick Lang <patrick.lang@hotmail.com>
2018-06-23 23:38:54 -0700

    Adding description field, comments to recipe schema

--- Recipe/Models/Recipe.cs
+++ Recipe/Models/Recipe.cs
@@ -10,15 +10,25 @@ namespace ogfg.recipeapp.Models
 {
     public class Recipe
     {
+        // generated
         public long Id { get; set; }
+
+        // generated / shared with shorturl
         public string ShortUrl { get; set; }
+
+        // Provided when recipe added
         public string Name { get; set; }
+        public string Description { get; set; }
+        public string LongUrl { get; set; }
+
+        // Optional when recipe added
+        public string BeerXml { get; set; }
+
+        // Used by ratings
         public long Rating1 { get; set; }
         public long Rating2 { get; set; }
         public long Rating3 { get; set; }
         public long Rating4 { get; set; }
         public long Rating5 { get; set; }
-        public string LongUrl { get; set; }
-        public string BeerXml { get; set; }
     }
 }
