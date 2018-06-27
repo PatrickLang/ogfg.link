157cfea099052e50103e29e1c05e5dbf609c95f8
Patrick Lang <patrick.lang@hotmail.com>
2018-06-27 00:26:12 -0700

    Starting db config

--- Recipe/Startup.cs
+++ Recipe/Startup.cs
@@ -23,8 +23,17 @@ namespace ogfg.recipeapp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<RecipeContext>(options =>
-             options.UseInMemoryDatabase("recipeTemp"));
+            var dbConnection = Configuration.GetConnectionString("DefaultConnection");
+            if (dbConnection != null)
+            {
+                services.AddDbContext<RecipeContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            }
+            else
+            {
+                services.AddDbContext<RecipeContext>(options =>
+                    options.UseInMemoryDatabase("recipeTemp"));
+            }
             services.AddMvc();
         }
 
