791e222cc38331a3175972e0efcd90e332cf50d6
Patrick Lang <patrick.lang@hotmail.com>
2018-06-30 20:59:31 -0700

    Adding more test links

--- shorturl/Controllers/DefaultController.cs
+++ shorturl/Controllers/DefaultController.cs
@@ -15,9 +15,25 @@ namespace shorturl.Controllers
 
         public DefaultController()
         {
+            string recipePrefix = "http://recipe.ogfg.link/Details?ShortUrl=";
             testValues = new Dictionary<string, string>();
             testValues.Add("bs", "http://beersmith.com/");
             testValues.Add("bx", "http://beerxml.com/");
+            testValues.Add("ktj3c", recipePrefix + "ktj3c");
+            testValues.Add("w6tyx", recipePrefix + "w6tyx");
+            testValues.Add("pq96x", recipePrefix + "pq96x");
+            testValues.Add("2hnmp", recipePrefix + "2hnmp");
+            testValues.Add("vb6md", recipePrefix + "vb6md");
+            testValues.Add("m3qw2", recipePrefix +"m3qw2");
+            testValues.Add("p7xvt", recipePrefix +"p7xvt");
+            testValues.Add("ae2br", recipePrefix +"ae2br");
+            testValues.Add("br2lx", recipePrefix +"br2lx");
+            testValues.Add("u4bxm", recipePrefix +"u4bxm");
+            testValues.Add("4fgpx", recipePrefix +"4fgpx");
+            testValues.Add("xt3wb", recipePrefix +"xt3wb");
+            testValues.Add("k7bwq", recipePrefix +"k7bwq");
+            testValues.Add("h29kz", recipePrefix +"h29kz");
+            testValues.Add("p5mwq", recipePrefix +"p5mwq");
         }
 
         [HttpGet("{id}")]
